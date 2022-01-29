28de66449ee3a486cb79a98da4faded72e70388f
primetimenumberline <primetimenumberline@gmail.comdion>
2022-01-29 08:19:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿long n = 10000;
+bool[] b = new bool[n];
+for (long i = 2; i < n; i++)
+    b[i] = true;
+for (long i = 2; i * i < n; i++)
+{
+    if ((b[i]))
+    {
+        for (long j = 2 * i; j < n; j += i)
+        {
+            b[j] = false;
+        }
+    }
+}
+for (long i = 2; i < n; i++)
+{
+    if (b[i])
+        Console.WriteLine("{0}\n", i.ToString());
+}
